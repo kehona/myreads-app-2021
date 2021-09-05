46e576d428b97da60f681960469e2f7e6641aad2
kenodipe <kenodipe@gmail.com>
2021-09-05 09:32:42 -0500

    #2 - State Manaagement

---
+++ src/App.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react'
+import Header from './components/Header'
+// import * as BooksAPI from './BooksAPI'
+import './App.css'
+import Shelves from './components/Shelves'
+
+const BooksApp = () => {
+
+
+  const initialBooks = [
+    {
+      id: 1,
+      url: "http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api",
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      shelf: "currentlyReading",
+    },
+    {
+      id: 2,
+      url: "http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api",
+      title: "Ender's Game",
+      author: "Orson Scott Card",
+      shelf: "currentlyReading",
+    },
+    {
+      id: 3,
+      url: "http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api",
+      title: "1776",
+      author: "David McCullough",
+      shelf: "wantToRead",
+    },
+    {
+      id: 4,
+      url: "http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api",
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      shelf: "wantToRead",
+    },
+    {
+      id: 5,
+      url: "http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api",
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      shelf: "read",
+    },
+    {
+      id: 6,
+      url: "http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api",
+      title: "Oh, the Places You'll Go!",
+      author: "Seuss",
+      shelf: "read",
+    },
+    {
+      id: 7,
+      url: "http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api",
+      title: "The Adventures of Tom Sawyer",
+      author: "Sawyer",
+      shelf: "read",
+    }
+  ]
+
+  /* TODO: Instead of using this state variable to keep track of which page
+  * we're on, use the URL in the browser's address bar. This will ensure that
+  * users can use the browser's back and forward buttons to navigate between
+  * pages, as well as provide a good URL they can bookmark and share.
+  */
+  const [showSearchPage, setShowSearchPage] = useState(false);
+
+  const [books, setBooks] = useState(initialBooks)
+
+  const updateBookShelf = (book, whereTo) => {
+    console.log(book)
+    console.log(whereTo)
+    const updatedBooks = books.map(b => {
+      if (b.id === book.id) {
+        book.shelf = whereTo;
+        return book;
+      }
+      return b;
+    })
+    setBooks(updatedBooks);
+  }
+
+  return (
+    <div className="app">
+      {console.log(books)}
+      {showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <button className="close-search" onClick={() => setShowSearchPage(false)}>Close</button>
+            <div className="search-books-input-wrapper">
+              {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+              <input type="text" placeholder="Search by title or author" />
+
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid"></ol>
+          </div>
+        </div>
+      ) : (
+        <div className="list-books">
+          <Header />
+          <div className="list-books-content">
+            <Shelves books={books} updateBookShelf={updateBookShelf} />
+          </div>
+          <div className="open-search">
+            <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+
+}
+
+export default BooksApp
